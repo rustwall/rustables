mullvad/nftnl-rs

Add Rule type and initial expressions

nftnl/src/expr/cmp.rs
@@ -1,0 +1,223 @@
+use libc;
+use nftnl_sys::{self as sys, c_void};
+
+use std::borrow::Cow;
+use std::ffi::CString;
+use std::slice;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+use super::Expression;
+use {ErrorKind, Result};
+
+/// Comparison operator.
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum CmpOp {
+    /// Equals.
+    Eq,
+    /// Not equal.
+    Neq,
+    /// Less than.
+    Lt,
+    /// Less than, or equal.
+    Lte,
+    /// Greater than.
+    Gt,
+    /// Greater than, or equal.
+    Gte,
+}
+
+impl CmpOp {
+    pub fn to_raw(&self) -> u32 {
+        use self::CmpOp::*;
+        match *self {
+            Eq => libc::NFT_CMP_EQ as u32,
+            Neq => libc::NFT_CMP_NEQ as u32,
+            Lt => libc::NFT_CMP_LT as u32,
+            Lte => libc::NFT_CMP_LTE as u32,
+            Gt => libc::NFT_CMP_GT as u32,
+            Gte => libc::NFT_CMP_GTE as u32,
+        }
+    }
+}
+
+
+/// Comparator expression. Allows comparing the content of the netfilter register with any value.
+pub struct Cmp<T: ToSlice> {
+    op: CmpOp,
+    data: T,
+}
+
+impl<T: ToSlice> Cmp<T> {
+    /// Returns a new comparison expression comparing the value loaded in the register with the
+    /// data in `data` using the comparison operator `op`.
+    pub fn new(op: CmpOp, data: T) -> Self {
+        Cmp { op, data }
+    }
+}
+
+impl<T: ToSlice> Expression for Cmp<T> {
+    fn to_expr(&self) -> Result<*mut sys::nftnl_expr> {
+        unsafe {
+            let expr = sys::nftnl_expr_alloc(b"cmp\0" as *const _ as *const i8);
+            if expr.is_null() {
+                bail!(ErrorKind::AllocationError);
+            }
+
+            let data = self.data.to_slice();
+            trace!("Creating a cmp expr comparing with data {:?}", data);
+
+            sys::nftnl_expr_set_u32(
+                expr,
+                sys::NFTNL_EXPR_CMP_SREG as u16,
+                libc::NFT_REG_1 as u32,
+            );
+            sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CMP_OP as u16, self.op.to_raw());
+            sys::nftnl_expr_set(
+                expr,
+                sys::NFTNL_EXPR_CMP_DATA as u16,
+                data.as_ref() as *const _ as *const c_void,
+                data.len() as u32,
+            );
+
+            Ok(expr)
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! nft_expr_cmp {
+    (==) => {
+        $crate::expr::CmpOp::Eq
+    };
+    (!=) => {
+        $crate::expr::CmpOp::Neq
+    };
+    (<) => {
+        $crate::expr::CmpOp::Lt
+    };
+    (<=) => {
+        $crate::expr::CmpOp::Lte
+    };
+    (>) => {
+        $crate::expr::CmpOp::Gt
+    };
+    (>=) => {
+        $crate::expr::CmpOp::Gte
+    };
+    ($op:tt $data:expr) => {
+        $crate::expr::Cmp::new(nft_expr_cmp!($op), $data)
+    };
+}
+
+
+/// A type that can be converted into a byte buffer.
+pub trait ToSlice {
+    /// Returns the data this type represents.
+    fn to_slice(&self) -> Cow<[u8]>;
+}
+
+impl<'a> ToSlice for [u8; 0] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Borrowed(&[])
+    }
+}
+
+impl<'a> ToSlice for &'a [u8] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Borrowed(self)
+    }
+}
+
+impl<'a> ToSlice for &'a [u16] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let ptr = self.as_ptr() as *const u8;
+        let len = self.len() * 2;
+        Cow::Borrowed(unsafe { slice::from_raw_parts(ptr, len) })
+    }
+}
+
+impl ToSlice for IpAddr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        match *self {
+            IpAddr::V4(ref addr) => addr.to_slice(),
+            IpAddr::V6(ref addr) => addr.to_slice(),
+        }
+    }
+}
+
+impl ToSlice for Ipv4Addr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(self.octets().to_vec())
+    }
+}
+
+impl ToSlice for Ipv6Addr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(self.octets().to_vec())
+    }
+}
+
+impl ToSlice for u8 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(vec![*self])
+    }
+}
+
+impl ToSlice for u16 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = (*self & 0x00ff) as u8;
+        let b1 = (*self >> 8) as u8;
+        Cow::Owned(vec![b0, b1])
+    }
+}
+
+impl ToSlice for u32 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = *self as u8;
+        let b1 = (*self >> 8) as u8;
+        let b2 = (*self >> 16) as u8;
+        let b3 = (*self >> 24) as u8;
+        Cow::Owned(vec![b0, b1, b2, b3])
+    }
+}
+
+impl ToSlice for i32 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = *self as u8;
+        let b1 = (*self >> 8) as u8;
+        let b2 = (*self >> 16) as u8;
+        let b3 = (*self >> 24) as u8;
+        Cow::Owned(vec![b0, b1, b2, b3])
+    }
+}
+
+impl<'a> ToSlice for &'a str {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::from(self.as_bytes())
+    }
+}
+
+/// Can be used to compare the value loaded by [`Meta::IifName`] and [`Meta::OifName`]. Please
+/// note that it is faster to check interface index than name.
+///
+/// [`Meta::IifName`]: enum.Meta.html#variant.IifName
+/// [`Meta::OifName`]: enum.Meta.html#variant.OifName
+pub enum InterfaceName {
+    /// Interface name must be exactly the value of the `CString`.
+    Exact(CString),
+    /// Interface name must start with the value of the `CString`.
+    ///
+    /// `InterfaceName::Wildcard("eth")` will look like `eth*` when printed and match against
+    /// `eth0`, `eth1`, ..., `eth99` and so on.
+    Wildcard(CString),
+}
+
+impl<'a> ToSlice for InterfaceName {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let bytes = match *self {
+            InterfaceName::Exact(ref name) => name.as_bytes_with_nul(),
+            InterfaceName::Wildcard(ref name) => name.as_bytes(),
+        };
+        Cow::from(bytes)
+    }
+}

nftnl/src/rule.rs
@@ -1,0 +1,84 @@
+use libc;
+use nftnl_sys::{self as sys, c_void};
+
+use chain::Chain;
+use expr::Expression;
+use {ErrorKind, MsgType, Result};
+
+pub struct Rule<'a> {
+    rule: *mut sys::nftnl_rule,
+    chain: &'a Chain<'a>,
+}
+
+impl<'a> Rule<'a> {
+    pub fn new(chain: &'a Chain) -> Result<Rule<'a>> {
+        unsafe {
+            let rule = sys::nftnl_rule_alloc();
+            if rule.is_null() {
+                bail!(ErrorKind::AllocationError);
+            }
+
+            sys::nftnl_rule_set_str(
+                rule,
+                sys::NFTNL_RULE_TABLE as u16,
+                chain.get_table().get_name().as_ptr(),
+            );
+            sys::nftnl_rule_set_str(
+                rule,
+                sys::NFTNL_RULE_CHAIN as u16,
+                chain.get_name().as_ptr(),
+            );
+            sys::nftnl_rule_set_u32(
+                rule,
+                sys::NFTNL_RULE_FAMILY as u16,
+                chain.get_table().get_family() as u32,
+            );
+
+            Ok(Rule { rule, chain })
+        }
+    }
+
+    pub fn set_position(&mut self, position: u64) {
+        unsafe {
+            sys::nftnl_rule_set_u64(self.rule, sys::NFTNL_RULE_POSITION as u16, position);
+        }
+    }
+
+    pub fn set_handle(&mut self, handle: u64) {
+        unsafe {
+            sys::nftnl_rule_set_u64(self.rule, sys::NFTNL_RULE_HANDLE as u16, handle);
+        }
+    }
+
+    pub fn add_expr<E: Expression>(&mut self, expr: E) -> Result<()> {
+        unsafe { sys::nftnl_rule_add_expr(self.rule, expr.to_expr()?) }
+        Ok(())
+    }
+
+    pub fn get_chain(&self) -> &Chain {
+        self.chain
+    }
+}
+
+unsafe impl<'a> ::NlMsg for Rule<'a> {
+    unsafe fn write(&self, buf: *mut c_void, seq: u32, msg_type: MsgType) {
+        let type_ = match msg_type {
+            MsgType::Add => libc::NFT_MSG_NEWRULE,
+            MsgType::Del => libc::NFT_MSG_DELRULE,
+        };
+        let nlh = sys::nftnl_nlmsg_build_hdr(
+            buf as *mut i8,
+            type_ as u16,
+            self.chain.get_table().get_family() as u16,
+            (libc::NLM_F_APPEND | libc::NLM_F_CREATE | libc::NLM_F_EXCL) as u16,
+            seq,
+        );
+        sys::nftnl_rule_nlmsg_build_payload(nlh, self.rule);
+    }
+}
+
+impl<'a> Drop for Rule<'a> {
+    fn drop(&mut self) {
+        unsafe { sys::nftnl_rule_free(self.rule) };
+    }
+}

rustwall/rustables

nftnl/src/expr/cmp.rs
@@ -1,0 +1,223 @@
+use libc;
+use nftnl_sys::{self as sys, c_void};
+
+use std::borrow::Cow;
+use std::ffi::CString;
+use std::slice;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+use super::Expression;
+use {ErrorKind, Result};
+
+/// Comparison operator.
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum CmpOp {
+    /// Equals.
+    Eq,
+    /// Not equal.
+    Neq,
+    /// Less than.
+    Lt,
+    /// Less than, or equal.
+    Lte,
+    /// Greater than.
+    Gt,
+    /// Greater than, or equal.
+    Gte,
+}
+
+impl CmpOp {
+    pub fn to_raw(&self) -> u32 {
+        use self::CmpOp::*;
+        match *self {
+            Eq => libc::NFT_CMP_EQ as u32,
+            Neq => libc::NFT_CMP_NEQ as u32,
+            Lt => libc::NFT_CMP_LT as u32,
+            Lte => libc::NFT_CMP_LTE as u32,
+            Gt => libc::NFT_CMP_GT as u32,
+            Gte => libc::NFT_CMP_GTE as u32,
+        }
+    }
+}
+
+
+/// Comparator expression. Allows comparing the content of the netfilter register with any value.
+pub struct Cmp<T: ToSlice> {
+    op: CmpOp,
+    data: T,
+}
+
+impl<T: ToSlice> Cmp<T> {
+    /// Returns a new comparison expression comparing the value loaded in the register with the
+    /// data in `data` using the comparison operator `op`.
+    pub fn new(op: CmpOp, data: T) -> Self {
+        Cmp { op, data }
+    }
+}
+
+impl<T: ToSlice> Expression for Cmp<T> {
+    fn to_expr(&self) -> Result<*mut sys::nftnl_expr> {
+        unsafe {
+            let expr = sys::nftnl_expr_alloc(b"cmp\0" as *const _ as *const i8);
+            if expr.is_null() {
+                bail!(ErrorKind::AllocationError);
+            }
+
+            let data = self.data.to_slice();
+            trace!("Creating a cmp expr comparing with data {:?}", data);
+
+            sys::nftnl_expr_set_u32(
+                expr,
+                sys::NFTNL_EXPR_CMP_SREG as u16,
+                libc::NFT_REG_1 as u32,
+            );
+            sys::nftnl_expr_set_u32(expr, sys::NFTNL_EXPR_CMP_OP as u16, self.op.to_raw());
+            sys::nftnl_expr_set(
+                expr,
+                sys::NFTNL_EXPR_CMP_DATA as u16,
+                data.as_ref() as *const _ as *const c_void,
+                data.len() as u32,
+            );
+
+            Ok(expr)
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! nft_expr_cmp {
+    (==) => {
+        $crate::expr::CmpOp::Eq
+    };
+    (!=) => {
+        $crate::expr::CmpOp::Neq
+    };
+    (<) => {
+        $crate::expr::CmpOp::Lt
+    };
+    (<=) => {
+        $crate::expr::CmpOp::Lte
+    };
+    (>) => {
+        $crate::expr::CmpOp::Gt
+    };
+    (>=) => {
+        $crate::expr::CmpOp::Gte
+    };
+    ($op:tt $data:expr) => {
+        $crate::expr::Cmp::new(nft_expr_cmp!($op), $data)
+    };
+}
+
+
+/// A type that can be converted into a byte buffer.
+pub trait ToSlice {
+    /// Returns the data this type represents.
+    fn to_slice(&self) -> Cow<[u8]>;
+}
+
+impl<'a> ToSlice for [u8; 0] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Borrowed(&[])
+    }
+}
+
+impl<'a> ToSlice for &'a [u8] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Borrowed(self)
+    }
+}
+
+impl<'a> ToSlice for &'a [u16] {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let ptr = self.as_ptr() as *const u8;
+        let len = self.len() * 2;
+        Cow::Borrowed(unsafe { slice::from_raw_parts(ptr, len) })
+    }
+}
+
+impl ToSlice for IpAddr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        match *self {
+            IpAddr::V4(ref addr) => addr.to_slice(),
+            IpAddr::V6(ref addr) => addr.to_slice(),
+        }
+    }
+}
+
+impl ToSlice for Ipv4Addr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(self.octets().to_vec())
+    }
+}
+
+impl ToSlice for Ipv6Addr {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(self.octets().to_vec())
+    }
+}
+
+impl ToSlice for u8 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::Owned(vec![*self])
+    }
+}
+
+impl ToSlice for u16 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = (*self & 0x00ff) as u8;
+        let b1 = (*self >> 8) as u8;
+        Cow::Owned(vec![b0, b1])
+    }
+}
+
+impl ToSlice for u32 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = *self as u8;
+        let b1 = (*self >> 8) as u8;
+        let b2 = (*self >> 16) as u8;
+        let b3 = (*self >> 24) as u8;
+        Cow::Owned(vec![b0, b1, b2, b3])
+    }
+}
+
+impl ToSlice for i32 {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let b0 = *self as u8;
+        let b1 = (*self >> 8) as u8;
+        let b2 = (*self >> 16) as u8;
+        let b3 = (*self >> 24) as u8;
+        Cow::Owned(vec![b0, b1, b2, b3])
+    }
+}
+
+impl<'a> ToSlice for &'a str {
+    fn to_slice(&self) -> Cow<[u8]> {
+        Cow::from(self.as_bytes())
+    }
+}
+
+/// Can be used to compare the value loaded by [`Meta::IifName`] and [`Meta::OifName`]. Please
+/// note that it is faster to check interface index than name.
+///
+/// [`Meta::IifName`]: enum.Meta.html#variant.IifName
+/// [`Meta::OifName`]: enum.Meta.html#variant.OifName
+pub enum InterfaceName {
+    /// Interface name must be exactly the value of the `CString`.
+    Exact(CString),
+    /// Interface name must start with the value of the `CString`.
+    ///
+    /// `InterfaceName::StartingWith("eth")` will look like `eth*` when printed and match against
+    /// `eth0`, `eth1`, ..., `eth99` and so on.
+    StartingWith(CString),
+}
+
+impl<'a> ToSlice for InterfaceName {
+    fn to_slice(&self) -> Cow<[u8]> {
+        let bytes = match *self {
+            InterfaceName::Exact(ref name) => name.as_bytes_with_nul(),
+            InterfaceName::StartingWith(ref name) => name.as_bytes(),
+        };
+        Cow::from(bytes)
+    }
+}

nftnl/src/rule.rs
@@ -1,0 +1,84 @@
+use libc;
+use nftnl_sys::{self as sys, c_void};
+
+use chain::Chain;
+use expr::Expression;
+use {ErrorKind, MsgType, Result};
+
+pub struct Rule<'a> {
+    rule: *mut sys::nftnl_rule,
+    chain: &'a Chain<'a>,
+}
+
+impl<'a> Rule<'a> {
+    pub fn new(chain: &'a Chain) -> Result<Rule<'a>> {
+        unsafe {
+            let rule = sys::nftnl_rule_alloc();
+            if rule.is_null() {
+                bail!(ErrorKind::AllocationError);
+            }
+
+            sys::nftnl_rule_set_str(
+                rule,
+                sys::NFTNL_RULE_TABLE as u16,
+                chain.get_table().get_name().as_ptr(),
+            );
+            sys::nftnl_rule_set_str(
+                rule,
+                sys::NFTNL_RULE_CHAIN as u16,
+                chain.get_name().as_ptr(),
+            );
+            sys::nftnl_rule_set_u32(
+                rule,
+                sys::NFTNL_RULE_FAMILY as u16,
+                chain.get_table().get_family() as u32,
+            );
+
+            Ok(Rule { rule, chain })
+        }
+    }
+
+    pub fn set_position(&mut self, position: u64) {
+        unsafe {
+            sys::nftnl_rule_set_u64(self.rule, sys::NFTNL_RULE_POSITION as u16, position);
+        }
+    }
+
+    pub fn set_handle(&mut self, handle: u64) {
+        unsafe {
+            sys::nftnl_rule_set_u64(self.rule, sys::NFTNL_RULE_HANDLE as u16, handle);
+        }
+    }
+
+    pub fn add_expr<E: Expression>(&mut self, expr: E) -> Result<()> {
+        unsafe { sys::nftnl_rule_add_expr(self.rule, expr.to_expr()?) }
+        Ok(())
+    }
+
+    pub fn get_chain(&self) -> &Chain {
+        self.chain
+    }
+}
+
+unsafe impl<'a> ::NlMsg for Rule<'a> {
+    unsafe fn write(&self, buf: *mut c_void, seq: u32, msg_type: MsgType) {
+        let type_ = match msg_type {
+            MsgType::Add => libc::NFT_MSG_NEWRULE,
+            MsgType::Del => libc::NFT_MSG_DELRULE,
+        };
+        let header = sys::nftnl_nlmsg_build_hdr(
+            buf as *mut i8,
+            type_ as u16,
+            self.chain.get_table().get_family() as u16,
+            (libc::NLM_F_APPEND | libc::NLM_F_CREATE | libc::NLM_F_EXCL) as u16,
+            seq,
+        );
+        sys::nftnl_rule_nlmsg_build_payload(header, self.rule);
+    }
+}
+
+impl<'a> Drop for Rule<'a> {
+    fn drop(&mut self) {
+        unsafe { sys::nftnl_rule_free(self.rule) };
+    }
+}